7561eddeaaf5f572530eeffde83d04282ebce2f1
Henry <henryppercy@gmail.com>
2022-11-04 14:37:32 +0000

    Added TODO

--- pig.php
+++ pig.php
@@ -3,6 +3,7 @@ require 'vendor/autoload.php';
 
 header('Content-Type: application/json; charset=utf-8'); // This will always contain json
 
+// TODO: Validate $_GET['id']
 $pigService = new FarmAPI\Services\PigService();
 $pig = $pigService->getPig($_GET['id']);
 
---
+++ src/DataAccess/PigDAO.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace FarmAPI\DataAccess;
+
+class PigDAO
+{
+
+}
