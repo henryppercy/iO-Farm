af9b4240c1245f7f13cdf8f89e09a3b053146d8a
Henry <henryppercy@gmail.com>
2022-11-04 12:52:07 +0000

    Added getters

--- src/Services/PigService.php
+++ src/Services/PigService.php
@@ -1,7 +1,26 @@
 <?php
 namespace FarmAPI\Services;
 
+use FarmAPI\DataAccess\Database;
+use FarmAPI\Entities\Pig;
+use FarmAPI\Entities\PigColl;
+
 class PigService
 {
-    
+    private Database $db;
+
+    public function __construct()
+    {
+        $this->db = Database::getInstance();
+    }
+
+    public function getPig(int $id): Pig
+    {
+        return new Pig();
+    }
+
+    public function getPigColl(): PigColl
+    {
+        return new PigColl();
+    }
 }
