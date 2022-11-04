0050f892fe8d4caee560ed2d97d1cc75b327201a
Henry <henryppercy@gmail.com>
2022-11-04 11:44:47 +0000

    Added getters and setters

--- src/Entities/Pig.php
+++ src/Entities/Pig.php
@@ -4,14 +4,94 @@ namespace FarmAPI\Entities;
 
 class Pig
 {
-    private int $is;
+    private int $id;
     private string $name;
     private float $weight;
     private string $colour;
     private int $species;
 
-    public function eat(string $food): string
+    /**
+     * @return int
+     */
+    public function getId(): int
     {
-        return 'Mmm ' . $food;
+        return $this->id;
     }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return float
+     */
+    public function getWeight(): float
+    {
+        return $this->weight;
+    }
+
+    /**
+     * @param float $weight
+     */
+    public function setWeight(float $weight): void
+    {
+        $this->weight = $weight;
+    }
+
+    /**
+     * @return string
+     */
+    public function getColour(): string
+    {
+        return $this->colour;
+    }
+
+    /**
+     * @param string $colour
+     */
+    public function setColour(string $colour): void
+    {
+        $this->colour = $colour;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSpecies(): int
+    {
+        return $this->species;
+    }
+
+    /**
+     * @param int $species
+     */
+    public function setSpecies(int $species): void
+    {
+        $this->species = $species;
+    }
+
+//    public function eat(string $food): string
+//    {
+//        return 'Mmm ' . $food;
+//    }
 }
