58a942f19e925871691b1940e6c23533ec168389
Henry <henryppercy@gmail.com>
2022-11-04 14:38:24 +0000

    Added DAO

--- src/DataAccess/PigDAO.php
+++ src/DataAccess/PigDAO.php
@@ -2,7 +2,14 @@
 
 namespace FarmAPI\DataAccess;
 
+use FarmAPI\Entities\Pig;
+use FarmAPI\Hydrators\PigHydrator;
+
 class PigDAO
 {
 
+    public static function fetch(Database $db, int $id): Pig
+    {
+        return PigHydrator::hydrateFromDb($db, $id);
+    }
 }
