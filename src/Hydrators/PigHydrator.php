bdc303ee47697e8fe89b808989fcb57015825132
Henry <henryppercy@gmail.com>
2022-11-04 14:38:46 +0000

    Created PigHydrator

--- src/Hydrators/PigHydrator.php
+++ src/Hydrators/PigHydrator.php
@@ -1,6 +1,27 @@
 <?php
+namespace FarmAPI\Hydrators;
+
+use FarmAPI\DataAccess\Database;
+use FarmAPI\Entities\Pig;
 
 class PigHydrator
 {
 
+    public static function hydrateFromDb(Database $db, int $id): Pig
+    {
+        $sql = 'SELECT `id`, `name`, `weight`, `colour`, `species` '
+            . 'FROM `pigs` '
+            . 'WHERE `id` = :id;';
+        $values = [':id' => $id];
+
+        $query = $db->getConnection()->prepare($sql);
+        $query->setFetchMode(\PDO::FETCH_CLASS, Pig::class);
+        $query->execute($values);
+
+        $pig = $query->fetch();
+        if (!$pig) {
+            throw new \InvalidArgumentException('Invalid ID');
+        }
+        return $pig;
+    }
 }
