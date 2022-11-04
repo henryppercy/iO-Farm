d41820c5925e5b062f3383a5b95008784fce329a
Henry <henryppercy@gmail.com>
2022-11-04 10:47:58 +0000

    Added template for error message ion index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php
+require 'vendor/autoload.php';
+
+$data = [
+    "message" => "Invalid route, please check the API documentation.",
+    "data" => []
+];
+
+echo '<p>' . $data['message'] . '</p>';
+echo json_encode($data, true);
