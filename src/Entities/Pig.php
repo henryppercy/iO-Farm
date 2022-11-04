d2924420444d0f171f14ccefa3a253ccbef45698
Henry <henryppercy@gmail.com>
2022-11-04 11:42:03 +0000

    Added properties to Pig class from DB

--- src/Entities/Pig.php
+++ src/Entities/Pig.php
@@ -4,6 +4,12 @@ namespace FarmAPI\Entities;
 
 class Pig
 {
+    private int $is;
+    private string $name;
+    private float $weight;
+    private string $colour;
+    private int $species;
+
     public function eat(string $food): string
     {
         return 'Mmm ' . $food;
