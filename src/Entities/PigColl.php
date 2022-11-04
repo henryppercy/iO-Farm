d3f38fd0c2b1185be0e58d17495d9230fba7ed74
Henry <henryppercy@gmail.com>
2022-11-04 12:20:29 +0000

    Created Pig collection class

--- src/Entities/PigColl.php
+++ src/Entities/PigColl.php
@@ -2,7 +2,24 @@
 
 namespace FarmAPI\Entities;
 
+// A collection of Pig objects
 class PigColl
 {
+    private array $pigs;
 
+    /**
+     * @return array
+     */
+    public function getPigs(): array
+    {
+        return $this->pigs;
+    }
+
+    /**
+     * @param array $pigs
+     */
+    public function setPigs(array $pigs): void
+    {
+        $this->pigs = $pigs;
+    }
 }
