d15281ca2ee28a8520b307bc015b22527510e73f
Henry <henryppercy@gmail.com>
2022-11-04 12:29:55 +0000

    Added outline to talk to backend

--- pigs.php
+++ pigs.php
@@ -3,9 +3,12 @@ require 'vendor/autoload.php';
 
 header('Content-Type: application/json; charset=utf-8'); // This will always contain json
 
+$pigService = new FarmAPI\Services\PigService();
+$pigColl = $pigService->getPigColl();
+
 $data = [
     "message" => "Successfully retrieved pigs",
-    "data" => ['Denis', 'Dave']
+    "data" => $pigColl->getPigs()
 ];
 
 echo json_encode($data, true);
