001859c875b1eeb9e5c05884346d5209ce694eb6
Henry <henryppercy@gmail.com>
2022-11-04 15:11:52 +0000

    Add error handling

--- pig.php
+++ pig.php
@@ -4,12 +4,26 @@ require 'vendor/autoload.php';
 header('Content-Type: application/json; charset=utf-8'); // This will always contain json
 
 // TODO: Validate $_GET['id']
-$pigService = new FarmAPI\Services\PigService();
-$pig = $pigService->getPig($_GET['id']);
+try {
+    $pigService = new FarmAPI\Services\PigService();
+    $pig = $pigService->getPig($_GET['id']);
 
-$data = [
-    "message" => "Successfully retrieved pig",
-    "data" => $pig
-];
+    $data = [
+        "message" => "Successfully retrieved pig",
+        "data" => $pig
+    ];
+} catch (InvalidArgumentException $exception) {
+    http_response_code(400);
+    $data = [
+        "message" => "Bad Request",
+        "data" => []
+    ];
+} catch (Exception $exception) {
+    http_response_code(500);
+    $data = [
+        "message" => "Bad Request",
+        "data" => []
+    ];
+}
 
 echo json_encode($data, true);
